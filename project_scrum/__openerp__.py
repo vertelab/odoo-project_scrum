fd5223b5bc0ea0525c250bd3571f66fb53a3906e
HaojunZou <apollo_zhj@msn.com>
2015-03-05 10:07:44 +0100

    version 1.4

--- project_scrum/__openerp__.py
+++ project_scrum/__openerp__.py
@@ -5,7 +5,6 @@
 #
 ##############################################################################
 
-
 {
     'name': 'Project Scrum',
     'version': '1.4',
@@ -20,13 +19,15 @@ More information:
     'website': 'http://www.vertel.se',
     'depends': ['project', 'mail'],
     'data': ['project_scrum_view.xml',
-    'wizard/project_scrum_test_task_view.xml',
-    'security/ir.model.access.csv',
-   ],
+        'wizard/project_scrum_test_task_view.xml',
+        'security/ir.model.access.csv',
+        'security/project_security.xml',
+       ],
     #'external_dependencies': {
         #'python' : ['bs4'],
     #},
     'demo': ['project_scrum_demo.xml'],
     'installable': True,
 }
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
