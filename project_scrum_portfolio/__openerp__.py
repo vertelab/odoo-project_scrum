84c3b1be7825ed24e411cef155be3b030d744960
Jakob Krabbe <jakob@syscare.se>
2019-05-21 18:10:35 +0200

    project_scrum_portfolio: new module

---
+++ project_scrum_portfolio/__openerp__.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#
+##############################################################################
+
+{
+    'name': 'Project Scrum Portfolio',
+    'version': '1.7',
+    'category': 'Project Management',
+    'description': """
+Adding Portfilio managent to Project Scrum
+==========================================
+
+Most organizations want or need to produce more than one product (focus 
+areas) at a time. These multiproduct organizations need to make economically 
+sound choices regarding how to manage the tradeoffs between their products. 
+One way to make economically sensible choices is to use 
+an agile portfolio planning process that aligns well with core agile 
+principles. For example can the webshop be one product (focus area) and 
+logisticts and manufacturing be other products. Some organizarions rather 
+work with focus areas than products, but the priciple is the same.
+
+Agile portfolio planning (or portfolio management) is an activity 
+for determining which products or projects to work on, in which order, 
+and for how long. If the same team are working with all these products, a 
+multiproduct team/project, Timeboxing aligns well with other agile principles.
+
+Timeboxing is allotting a fixed, maximum unit of time for an activity or 
+several activities of the same type. That unit of time is called a time box. 
+The goal of timeboxing is to define and limit the amount of time dedicated 
+to an activity. For example are a sprint a type of Timebox. In this case
+a Sprint can be divied in several timeboxes where each timebox represent
+resources allotted for a product or focus area.
+
+* Products and/or focus areas for planning and monitoring in a Portfolio
+* Split a sprint in several timeboxes / products / focus areas
+* Attach activites to a timebox / focus area / product
+* Color an actrivity from a timebox / focus area / Product
+
+Agile portfolio planning should include an appropriate set of internal 
+stakeholders, who have the perspective to properly prioritize new products 
+and make decisions regarding in-process products. It should also include the 
+product owners of individual products, who act as champions for their products 
+and advocates for resources. 
+
+https://innolution.com/essential-scrum/table-of-contents/chapter-16-portfolio-planning
+
+    """,
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': ['project_scrum'],
+    'data': ['project_scrum_view.xml',
+#        'security/ir.model.access.csv',
+#        'security/project_security.xml',
+       ],
+    #'external_dependencies': {
+        #'python' : ['bs4'],
+    #},
+    # ~ 'demo': ['project_scrum_demo.xml'],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ project_scrum_portfolio/project_scrum.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2019- Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+#from bs4 import BeautifulSoup
+import openerp.tools
+import re
+import time
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class scrum_sprint_portfolio(models.Model):
+    _name = 'project.scrum.portfolio'
+    _inherit = ['mail.thread']
+    _description = 'Project Scrum Portfolio'
+    _order = 'name desc'
+
+    name = fields.Char(string = 'Portfolio Name', required=True,help='Product or focus area')
+    description = fields.Text(string = 'Description', required=False)
+    color = fields.Integer(string='Color Index')
+    # ~ project_ids = fields.One2many(comodel_name='project.project', invserse_name = 'portfolio_id')
+    # ~ sprint_ids = fields.One2many(comodel_name="project.sprint",invserse_name = 'portfolio_id')
+    # ~ task_ids = fields.One2many(comodel_name="project.task",inverse_name = 'portfolio_id')
+    # ~ @api.one
+    # ~ def _task_count(self):
+        # ~ self.task_count = len(self.task_ids)
+    # ~ task_count = fields.Integer(compute = '_task_count')
+
+    timebox_ids = fields.One2many(comodel_name="project.scrum.timebox",inverse_name = 'portfolio_id')
+    user_id = fields.Many2one(comodel_name='res.users', string='Product Owner',help="Manager for a product or Focus Area")
+    
+    @api.one
+    def _planned_hours(self):
+        # ~ self.planned_hours =  sum(self.timebox_ids.filtered(lambda tb:  tb.sprint_id and tb.sprint_id.state == 'draft').mapped('planned_hours'))
+        self.planned_hours =  sum(self.timebox_ids.mapped('planned_hours'))
+        # ~ self.consumed_hours =  sum(self.timebox_ids.filtered(lambda tb: tb.sprint_id and tb.sprint_id.state == 'done').mapped('planned_hours'))
+        self.consumed_hours =  sum(self.timebox_ids.mapped('planned_hours'))
+    planned_hours = fields.Float(compute="_planned_hours",group_operator="sum", string='Planned Hours', help="Hours timedboxed for sprints planned",readonly=True)
+    consumed_hours = fields.Float(compute="_planned_hours",group_operator="sum", string='Consumed Hours', help="Hours consumed for done sprints",readonly=True)
+    
+    # ~ @api.one
+    # ~ def _progress(self):
+        # ~ if self.planned_hours and self.effective_hours:
+            # ~ self.progress = self.effective_hours / self.planned_hours * 100
+    # ~ progress = fields.Float(compute="_progress", group_operator="avg", string='Progress (0-100)', help="Computed as: Time Spent / Total Time.")
+      
+
+
+class scrum_sprint_timebox(models.Model):
+    _name = 'project.scrum.timebox'
+    _description = 'Project Scrum Timebox'
+
+    # ~ name = fields.Char(string = 'Timebox', required=True)
+    portfolio_id = fields.Many2one(comodel_name='project.scrum.portfolio')
+    sprint_id = fields.Many2one(comodel_name="project.sprint")
+    # ~ task_ids = fields.One2many(comodel_name="project.task",inverse_name = 'timebox_id')
+    
+    planned_hours = fields.Float(string="Planned Hours")
+    
+    @api.one
+    def _sprint_share(self):
+        self.sprint_share = 10.0
+        #self.sprint_share = round(self.planned_hours / (self.sprint_id.planned_hours if self.sprint_id and self.sprint_id.planned_hours else 1.0) * 100,0) 
+    sprint_share = fields.Float(compute="_sprint_share",readonly=True)
+    
+
+    # ~ @api.one
+    # ~ def _progress(self):
+        # ~ if self.planned_hours and self.effective_hours:
+            # ~ self.progress = self.effective_hours / self.planned_hours * 100
+    # ~ progress = fields.Float(compute="_progress", group_operator="avg", string='Progress (0-100)', help="Computed as: Time Spent / Total Time.")
+
+class scrum_sprint(models.Model):
+    _inherit = 'project.scrum.sprint'
+
+    timebox_ids = fields.One2many(comodel_name="project.scrum.timebox",inverse_name = 'sprint_id')
+
+class project_task(models.Model):
+    _inherit = "project.task"
+
+    color = fields.Integer(string='Color Index',related='portfolio_id.color')
+    portfolio_id = fields.Many2one(comodel_name="project.scrum.portfolio")
+    # ~ timebox_id = fields.Many2one(comodel_name="project.scrum.timebox")
+
+    # ~ @api.model
+    # ~ def _read_group_sprint_type(self,ids,domain,**kwarg):
+        # ~ return self.env['project.sprint.type'].search([]).name_get(), {}
+
+    # ~ _group_by_full = {
+        # ~ 'portfolio_id': _read_group_sprint_id,
+        # ~ 'sprint_id': _read_group_sprint_id,
+        # ~ 'us_id': _read_group_us_id,
+        # ~ 'stage_id': _read_group_stage_ids,
+        # ~ 'user_id': _read_group_user_id,
+        # ~ 'sprint_type': _read_group_sprint_type,
+    # ~ }
+
