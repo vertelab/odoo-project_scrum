5a94d1841803cfc3cd85e00de4a05ec1db17dd00
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-08-15 08:36:17 +0200

    Added more info in the description

--- project_task_id/__manifest__.py
+++ project_task_id/__manifest__.py
@@ -7,11 +7,15 @@
     'summary': 'Adds id to task',
     'description': """
 This module is maintained from https://github.com/vertelab/odoo-project_scrum/tree/14.0/project_task_id. \n
-Adds an id to task using a counter on each projekt.
+Adds an id to task using a counter on each projekt. \n
+It can give one global sequence or one sequence per project. \n
+Toggle which to use through Project -> Configuration -> Settings -> Only use one task sequence 
+
+This module i fairly similar to https://github.com/OCA/project/tree/14.0/project_key. \n
     """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
-    'website': 'http://www.vertel.se',
+    'website': 'https://www.vertel.se',
     'depends': ['project'],
     'data': [
         'views/project_view.xml',
