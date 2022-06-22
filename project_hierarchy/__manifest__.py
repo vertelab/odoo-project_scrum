a0891f9f74cfbc7c163028eb86addf8c6e5d43d8
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-22 09:57:54 +0200

    Update __manifest__.py
    fixing the manifest

--- project_hierarchy/__manifest__.py
+++ project_hierarchy/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Project Scrum: Project Forecast CE',
+    'name': 'Project Scrum: Project Hierarchy',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Forecast your projects',
@@ -28,7 +28,7 @@
     'description': '>> Description',
     #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-project-scrum/project_forecast_ce',
+    'website': 'https://vertel.se/apps/odoo-project-scrum/project_hierarchy',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
