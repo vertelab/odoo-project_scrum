5bf0971576ea9921f7224352fa1aff0100883482
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-22 16:31:14 +0200

    Update __manifest__.py
    fixing with manifest

--- project_scrum_portfolio/__manifest__.py
+++ project_scrum_portfolio/__manifest__.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Project Scrum Portfolio',
-    'version': '1.9',
-    'category': 'Project Management',
+    'name': 'Project Scrum: Scrum Portfolio',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'UAdding Portfilio management to Project Scrum.',
+    'category': 'Productivity',
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-project-scrum/project_scrum_portfolio',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-project-scrum',
     'description': """
 Adding Portfilio management to Project Scrum
 ==========================================
