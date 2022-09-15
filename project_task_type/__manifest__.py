3da5361223053f9192510d25e1f90c1c5a2f639f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-15 09:50:05 +0200

    Update __manifest__.py

--- project_task_type/__manifest__.py
+++ project_task_type/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds type to task.',
-    'category': 'Productivity',
+    'category': 'Project',
     'description': 'Adds a type to task.',
     #'sequence': '1',
     'author': 'Vertel AB',
@@ -33,7 +33,7 @@
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-project-scrum',
+    'repository': 'https://github.com/vertelab/odoo-project_scrum',
     'depends': ['project'],
     'data': [
         'project_view.xml',
