233b515fc779b5f83a35224d348857babfcddb4c
iammiracle <miracleaayodele@gmail.com>
2025-03-11 14:43:35 +0100

    fixing p-files

--- project_sprint_module/__manifest__.p.py
+++ project_sprint_module/__manifest__.p.py
@@ -21,7 +21,11 @@
 
 {
     'name': 'Project Scrum: Project Sprint Module',
-    'version': '14.0.0.0.0',
+    # # if VERSION ==  "14.0"
+    'version': '14.0.1.24.0',
+    # # else
+    'version': '1.24',
+    # # endif
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds new page for module list.',
     'category': 'Project',
