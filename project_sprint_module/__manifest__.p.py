c327931ec9a97fa27427c4bf48142225d154dc5c
vertelbot <vertelbot@vertel.se>
2025-03-11 13:48:02 +0000

    odoobranchpfile odoo-project_scrum from 14.0. Do not p-file sync

--- project_sprint_module/__manifest__.p.py
+++ project_sprint_module/__manifest__.p.py
@@ -21,8 +21,11 @@
 
 {
     'name': 'Project Scrum: Project Sprint Module',
-    'version': '14.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    # # if VERSION ==  "14.0"
+    'version': '14.0.1.24.0',
+    # # else
+    'version': '1.24',
+    # # endif
     'summary': 'Adds new page for module list.',
     'category': 'Project',
     'description': """
