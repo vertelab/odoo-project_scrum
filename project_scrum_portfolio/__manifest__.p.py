e4c98fabd58d4c9224922aa50669064b70ec4b7c
vertelbot <vertelbot@vertel.se>
2025-03-11 15:15:29 +0000

    odoobranchpfile odoo-project_scrum from 18.0. Do not p-file sync

--- project_scrum_portfolio/__manifest__.p.py
+++ project_scrum_portfolio/__manifest__.p.py
@@ -21,7 +21,11 @@
 
 {
     'name': 'Project Scrum: Scrum Portfolio',
+    # # if VERSION ==  "14.0"
     'version': '14.0.0.0.0',
+    # # else
+    'version': '1.0',
+    # # endif
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adding Portfilio management to Project Scrum.',
     'category': 'Project',
--- project_scrum_portfolio/__manifest__.py
+++ project_scrum_portfolio/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Project Scrum: Scrum Portfolio',
-    'version': '14.0.0.0.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adding Portfilio management to Project Scrum.',
     'category': 'Project',
