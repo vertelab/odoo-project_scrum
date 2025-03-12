e05e6691f4d6a4396a26001dcf3a84752484d482
vertelbot <vertelbot@vertel.se>
2025-03-12 08:09:54 +0000

    odoobranchpfile odoo-project_scrum from 18.0. Do not p-file sync

--- project_scrum/models/project_scrum.py
+++ project_scrum/models/project_scrum.py
@@ -343,7 +343,7 @@ class ProjectUserStories(models.Model):
         return None
 
     @api.model
-    def _read_group_sprint_id(self, present_ids, domain, **kwargs):
+    def _read_group_sprint_id(self, present_ids, domain):
         project_id = self._resolve_project_id_from_context()
         sprints = self.env['project.scrum.sprint'].search([
             ('project_id', '=', project_id)], order='sequence').name_get()
@@ -485,11 +485,9 @@ class ProjectTask(models.Model):
             if not self.sprint_ids or not vals.get('sprint_id') in self.sprint_ids.mapped('id'):
                 self.sprint_ids = [(4, vals.get('sprint_id'), 0)]
         return super(ProjectTask, self).write(vals)
-    
-    def _read_group_sprint_id(self, sprint_id, domain, order):
-        sprint_ids = sprint_id._search([
-            ('project_id', '=', self.project_id.id)
-        ], order='date_start asc', access_rights_uid=SUPERUSER_ID)
+
+    def _read_group_sprint_id(self, sprint_id, domain):
+        sprint_ids = sprint_id.sudo()._search([('project_id', '=', self.project_id.id)], order='date_start asc')
         return sprint_id.browse(sprint_ids)
 
     # Not sure what this is for. Keep here
