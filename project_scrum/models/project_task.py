f9a1f87b96f2682ed1a9da414a0ccef37f4382f7
vertelbot <vertelbot@vertel.se>
2025-03-19 10:20:24 +0000

    odoobranchpfile odoo-project_scrum from 14.0. Do not p-file sync

--- project_scrum/models/project_task.py
+++ project_scrum/models/project_task.py
@@ -144,8 +144,6 @@ class ProjectTask(models.Model):
             # Add the filter for sprint_id (not active_sprint_id)
             # We need to be careful here - we're in the project.task model,
             # so we should be filtering on sprint_id, which is a field in project.task
-            # Create a domain that includes tasks with sprint_id in allowed_sprint_ids
-            # OR tasks with no sprint_id (undefined)
             domain = expression.AND([
                 domain,
                 [
