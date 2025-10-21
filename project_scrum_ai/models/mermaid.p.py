e73293488bad0b1d86b3381203d684487e2f9b53
iammiracle <miracleaayodele@gmail.com>
2025-10-21 10:20:02 +0200

    [T/6282]project_scrum_ai: added ability to created mermaid chat with AI

---
+++ project_scrum_ai/models/mermaid.p.py
@@ -0,0 +1,31 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError, AccessError
+import re
+import html
+from bs4 import BeautifulSoup
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+from odoo import models, fields
+
+
+class MermaidMixin(models.AbstractModel):
+    _inherit = 'mermaid.mixin'
+
+    def _process_mermaid_prompt(self):
+        try:
+            quest_id = self.env.ref('project_scrum_ai.mermaid_ai_quest')
+            result = quest_id.run(prompt=self._mermaid_prompt(), record=self)
+
+            if result:
+                ai_messages = quest_id._get_last_ai_message(result.get('result', {}).get('messages', False))
+                if not ai_messages:
+                    raise UserError(
+                        _("OBS: An error occurred, you should contact administrator to look into the quest"))
+
+                return ai_messages.content
+        except Exception as e:
+            _logger.warning(f"Error: {e}")
