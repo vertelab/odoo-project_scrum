4245f569021670d46d3eb3a14578c889f600cd79
Waland <waland@vertel.se>
2026-07-01 12:29:32 +0000

    fix: post_init_hook uses scan_filesystem (not import_installed_modules)

--- project_sprint_module/__init__.py
+++ project_sprint_module/__init__.py
@@ -1,5 +1,5 @@
 from . import models
 
 def post_init_hook(env):
-    """Förladda sprint.module med alla installerade Odoo-moduler vid installation."""
-    env["sprint.module"].import_installed_modules()
+    """Scanna filsystemet efter odoo-* repon och skapa sprint.module-poster."""
+    env["sprint.module"].scan_filesystem()
