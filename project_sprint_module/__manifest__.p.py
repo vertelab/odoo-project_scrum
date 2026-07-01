d1ff4ccb44e5d3799c88018505ccad3d024816f5
vertelbot <vertelbot@vertel.se>
2026-07-01 12:52:31 +0000

    odoobranchpfile odoo-project_scrum from 18.0. Do not p-file sync

--- project_sprint_module/__manifest__.p.py
+++ project_sprint_module/__manifest__.p.py
@@ -39,7 +39,6 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-project_scrum',
-    # #if VERSION >= "18.0"
     'depends': ['project', 'project_scrum', 'mail'],
     'data': [
         'security/ir.model.access.csv',
@@ -55,15 +54,11 @@
             'project_sprint_module/static/src/css/module_list.css',
         ],
     },
-    # #else
     'depends': ['project', 'project_scrum'],
     'data': ['views/project_sprint_module.xml'],
-    # #endif
     'demo': [],
     'installable': True,
-    # #if VERSION >= "18.0"
     'post_init_hook': 'post_init_hook',
-    # #endif
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
