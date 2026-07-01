3b884d697b93e736fac8c8ee202c339578ab950c
vertelbot <vertelbot@vertel.se>
2026-07-01 12:33:19 +0000

    odoobranchpfile odoo-project_scrum from 18.0. Do not p-file sync

--- project_sprint_module/__init__.p.py
+++ project_sprint_module/__init__.p.py
@@ -2,6 +2,6 @@ from . import models
 
 # #if VERSION >= "18.0"
 def post_init_hook(env):
-    """Förladda sprint.module med alla installerade Odoo-moduler vid installation."""
-    env["sprint.module"].import_installed_modules()
+    """Scanna filsystemet efter odoo-* repon och skapa sprint.module-poster."""
+    env["sprint.module"].scan_filesystem()
 # #endif
